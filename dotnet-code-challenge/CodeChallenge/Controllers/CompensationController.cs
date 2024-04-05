f27b799d995720c1dde6dc3470bd206d4a57ed56
unknown <junehah90@gmail.com>
2024-04-05 12:45:21 -0400

    initial working of reporting

---
+++ dotnet-code-challenge/CodeChallenge/Controllers/CompensationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodeChallenge.Services;
+using CodeChallenge.Models;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/compensation")]
+    public class CompensationController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public CompensationController(ILogger<CompensationController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensationByEmployeeId([FromBody] Employee employee, double salary, DateTime effectiveDate)
+        {
+            _logger.LogDebug($"Received compensation create request for '{employee}'");
+
+            _employeeService.Create(employee);
+
+            return CreatedAtRoute("getEmployeeById", new { id = employee.EmployeeId }, employee);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationByEmployeeId")]
+        public IActionResult GetCompensationByEmployeeId(String id)
+        {
+            _logger.LogDebug($"Received compensation get request for '{id}'");
+
+            var employee = _employeeService.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ dotnet-code-challenge/CodeChallenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,61 @@
+﻿using CodeChallenge.Models;
+using CodeChallenge.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/reporting")]
+    public class ReportingStructureController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public ReportingStructureController(ILogger<CompensationController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id}", Name = "getReportingStructure")]
+        public IActionResult GetReportingStructure(String id)
+        {
+            _logger.LogDebug($"Received reporting structure get request for '{id}'.");
+            var employee = _employeeService.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            var reportingNum = 0;
+
+            if (employee.DirectReports is not null)
+            {
+                reportingNum = employee.DirectReports.Count();
+                foreach (var x in employee.DirectReports)
+                {
+                    //update cuz this is disgusting.  
+                    //any child employee does not return their direct reports.  
+                    var tempEmployee = _employeeService.GetById(x.EmployeeId);
+                    if (tempEmployee.DirectReports.Any())
+                    {
+                        reportingNum += x.DirectReports.Count();
+                    }
+                    //may not need this as needed as it might return an employee object for each directReport
+                    //if (x.DirectReports is not null)
+                    //    reportingNum += x.DirectReports.Count();
+                }
+            }
+
+            var reportingStructure = new ReportingStructure
+            {
+                Employee = employee,
+                NumberOfReports = reportingNum
+            };
+
+            return Ok(reportingStructure);
+        }
+    }
+}
